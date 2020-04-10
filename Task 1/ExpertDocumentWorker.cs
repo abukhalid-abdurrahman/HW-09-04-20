91fbd2c52bdda22ba0953fee35b543569c6b1dd3
unknown <madysoftware@gmail.com>
2020-04-10 17:08:14 +0500

    Класс ExpertDocumentWorker был добавлен

---
+++ Task 1/ExpertDocumentWorker.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace  Task_1
+{
+    public class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+            base.SaveDocument();
+        }
+    }
+}
