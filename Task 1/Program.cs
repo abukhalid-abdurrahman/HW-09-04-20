d24185656760a77b5b024694c0fc60776bd8b0ae
unknown <madysoftware@gmail.com>
2020-04-10 17:19:21 +0500

    Ввод ключа пользователем через функцию Main

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -6,6 +6,9 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите ключ доступа.\nПропустите этот этап в случае отказа от расширенной версии,\nи хотите использовать бесплатную версию.");
+            Console.Write("Введите ключ: ");
+            string key = Console.ReadLine();
             
         }
     }
